fa3f405be18d1114c4b6b17d517a093b15a2896c
vrndarani <98180585+vrndarani@users.noreply.github.com>
2022-04-12 13:24:57 +0300

    menu navtoggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-console.log("Hi and welcome to my website!")
+function menuToggle() {
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }}
